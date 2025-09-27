7fcdb8199e529e9b33cd8cc80b2146f9a8e3f537
Yasashvee Karthi <yasashvee@gmail.com>
2025-09-27 15:10:57 -0500

    more updating for win screen

--- win_zone.gd
+++ win_zone.gd
@@ -3,12 +3,16 @@ extends Area2D
 signal player_won
 
 func _ready() -> void:
+	# Make sure the Area2D can detect bodies
+	monitoring = true
 	# Connect the body_entered signal to handle when player enters the zone
 	body_entered.connect(_on_body_entered)
+	print("Win zone is ready and monitoring")
 
 func _on_body_entered(body: Node2D) -> void:
 	# Check if the body that entered is the player
-	if body.name == "player":
+	print("Body entered win zone: ", body.name)
+	if body is CharacterBody2D and body.name == "player":
 		print("Player reached the win zone!")
 		player_won.emit()
 		# Change scene to winning screen
