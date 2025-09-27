2834f3eb0788a6b1e69f7501c2744c57ace4327d
Solus-QE <solus@phanguard.org>
2025-09-27 14:07:18 -0500

    added main animated sprite

--- player.gd
+++ player.gd
@@ -1,25 +1,40 @@
 extends CharacterBody2D
 
+@onready var anim = $AnimatedSprite2D
 
 const SPEED = 1500.0
 const JUMP_VELOCITY = -1500.0
 
+func _ready() -> void:
+	anim.play("idle")
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	# Jump
 	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Left / Right movement
 	var direction := Input.get_axis("left", "right")
-	if direction:
+	if direction != 0:
 		velocity.x = direction * SPEED
+		anim.flip_h = direction < 0
+
+		if is_on_floor():
+			anim.play("walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor():
+			anim.play("idle")
+
+	# Jump/Fall animation
+	if not is_on_floor():
+		if velocity.y < 0:
+			anim.play("jump")
+		else:
+			anim.play("fall")
 
 	move_and_slide()
